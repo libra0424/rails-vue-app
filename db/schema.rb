4531ccef2450bc7fc69eeefc117a7d01c76b32ad
ryotana <officework.tanaka@gmail.com>
2021-10-07 01:06:17 +0900

    feat: 従業員モデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_06_160315) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "department", default: "", null: false
+    t.integer "gender", default: 0, null: false
+    t.date "birth"
+    t.date "joined_date"
+    t.bigint "payment", default: 0, null: false
+    t.text "note", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "lock_version"
+  end
 
 end
