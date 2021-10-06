9a14d2b2ee0f0139f575771464a6fc26d5ce2529
ryotana <officework.tanaka@gmail.com>
2021-10-07 01:13:08 +0900

    refactor: rubocopを追加

--- db/migrate/20211006160315_create_employees.rb
+++ db/migrate/20211006160315_create_employees.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
     create_table :employees do |t|
-      t.string :name, null: false, default: ""
-      t.string :department, null: false, default: ""
+      t.string :name, null: false, default: ''
+      t.string :department, null: false, default: ''
       t.integer :gender, null: false, default: 0
       t.date :birth, null: true
       t.date :joined_date, null: true
       t.bigint :payment, null: false, default: 0
-      t.text :note, null: false, default: ""
+      t.text :note, null: false, default: ''
 
       t.timestamps
       t.integer :lock_version
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,19 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_160315) do
-
-  create_table "employees", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "department", default: "", null: false
-    t.integer "gender", default: 0, null: false
-    t.date "birth"
-    t.date "joined_date"
-    t.bigint "payment", default: 0, null: false
-    t.text "note", default: "", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "lock_version"
+ActiveRecord::Schema.define(version: 20_211_006_160_315) do
+  create_table 'employees', force: :cascade do |t|
+    t.string 'name', default: '', null: false
+    t.string 'department', default: '', null: false
+    t.integer 'gender', default: 0, null: false
+    t.date 'birth'
+    t.date 'joined_date'
+    t.bigint 'payment', default: 0, null: false
+    t.text 'note', default: '', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'lock_version'
   end
-
 end
