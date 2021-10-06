20536e7103cc730ba5aff8984c31adf72e46d049
ryotana <officework.tanaka@gmail.com>
2021-10-07 01:23:15 +0900

    feat: employeeモデルのバリデーションを追加

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class Employee < ApplicationRecord
+  GENDERS = { other: 0, male: 1, female: 2 }.freeze
+
+  enum gender: GENDERS
+
+  validates :gender, inclusion: { in: GENDERS.keys.concat(GENDERS.keys.map(&:to_s)) }, exclusion: { in: [nil] }
+  validates :name, exclusion: { in: [nil, ''] }
+  validates :department, exclusion: { in: [nil] }
+  validates :payment, numericality: true, exclusion: { in: [nil] }
+  validates :note, exclusion: { in: [nil] }
 end
